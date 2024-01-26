a9534a7ec7eca8d6dc83a6ed01fe9694b545e398
karl <karl@studio206.dev>
2024-01-26 13:12:00 +0000

    add avoid escape

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next"],
   plugins: ["@stylistic"],
   rules: {
-    "@stylistic/quotes": [2, "double"],
+    "@stylistic/quotes": [2, "double", { avoidEscape: true }],
     "@typescript-eslint/no-unused-vars": "error",
   },
 };
