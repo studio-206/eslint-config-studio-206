ed782addc75b180d67a2d4019aa3cca7c1f135d5
karl <karl@studio206.dev>
2024-01-26 13:01:46 +0000

    Get it working

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["eslint:recommended", "next"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next"],
+  plugins: ["@stylistic"],
+  rules: {
+    "@stylistic/quotes": [2, "double"],
+  },
 };
