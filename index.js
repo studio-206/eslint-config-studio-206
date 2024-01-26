ffe9d7c8228422f691a2c51cce21f1c2426eb801
karl <karl@studio206.dev>
2024-01-26 17:56:53 +0000

    remove next

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "next"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   plugins: ["@stylistic"],
   rules: {
     "@stylistic/quotes": [2, "double", { avoidEscape: true }],
